097fe75482ca14411fa0a71a22b305f168ccea06
Jonathan Neugebauer <jonathan.n@uni-muenster.de>
2020-03-03 10:53:20 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:lts-alpine
+
+WORKDIR /usr/src/app
+
+ARG VERSION
+RUN BWIP_VERSION=VERSION npm run dependencies
+
+COPY . .
+
+EXPOSE 8080
+CMD [ "node", "server.js" ]
---
+++ server.js
@@ -0,0 +1,15 @@
+// Simple HTTP server that renders barcode images using bwip-js.
+const http   = require('http');
+const bwipjs = require('bwip-js');
+
+http.createServer(function(req, res) {
+    // If the url does not begin /?bcid= then 404.  Otherwise, we end up
+    // returning 400 on requests like favicon.ico.
+    if (req.url.indexOf('/?bcid=') != 0) {
+        res.writeHead(404, { 'Content-Type':'text/plain' });
+        res.end('BWIPJS: Unknown request format.', 'utf8');
+    } else {
+        bwipjs.request(req, res); // Executes asynchronously
+    }
+
+}).listen(8080);
